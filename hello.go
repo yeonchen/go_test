76e9992fbdfc6d5dddadd2188ba1aec0e5c88bf4
xerros1028 <xerros1028@gmail.com>
2024-09-05 12:04:44 +0900

    처음올려봄

---
+++ hello.go
@@ -0,0 +1,19 @@
+package main
+
+import "fmt"
+
+// func main() {
+// 	fmt.Println("hello, 세계")
+// }
+
+// func main() {
+// 	var result int
+// 	result = 1 + 2
+// 	fmt.Println(result)
+// }
+
+func main() {
+	fmt.Println("hello, 세계")
+	fmt.Print("hello, ", "I'm min ")
+	fmt.Printf("%d years old!", 24)
+}
